c8e62e26d1a36f71e7fdff4b7144d99d341a9ad3
rodgers125 <korirrodgers103@gmail.com>
2023-10-25 14:29:05 +0300

    correction

--- school/urls.py
+++ school/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("teachers.urls")),
+    path('teachers/', include("teachers.urls")),
     path('students/', include("students.urls"))
 ]
